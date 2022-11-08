be662e96763906320623e187d5b8ff8c20fbc42d
nishi-ai <aiaimforworld@gmail.com>
2022-11-08 09:06:25 +0100

    implement fade in function with Intersection Observer API

--- js/main.js
+++ js/main.js
@@ -32,21 +32,50 @@ $(function () {
     variableWidth: true, // auto calculation the width for each slide
   });
 
-  // Fadein
-  $(window).scroll(function () {
-    $(".fadein").each(function () {
-      // get current scrolled height
-      let scroll = $(window).scrollTop();
-      // get the height until each fadein element
-      let target = $(this).offset().top;
-      // get window height itself
-      let windowHeight = $(window).height();
-      // start Fade in when the scrolled height is bigger than the target height -
-      // window's height + 200px
-      if (scroll > target - windowHeight + 200) {
-        $(this).css("opacity", "1");
-        $(this).css("transform", "translateY(0)");
-      }
-    });
+  // Fadein with jQuary
+  //   $(window).scroll(function () {
+  //     $(".fadein").each(function () {
+  //       // get current scrolled height
+  //       let scroll = $(window).scrollTop();
+  //       // get the height until each fadein element
+  //       let target = $(this).offset().top;
+  //       // get window height itself
+  //       let windowHeight = $(window).height();
+  //       // start Fade in when the scrolled height is bigger than the target height -
+  //       // window's height + 200px
+  //       if (scroll > target - windowHeight + 200) {
+  //         $(this).css("opacity", "1");
+  //         $(this).css("transform", "translateY(0)");
+  //       }
+  //     });
+  //   });
+});
+
+// Fadein with Intersection Observer API
+const fadeTargets = document.querySelectorAll(".fadein");
+
+const fadeOption = {
+  root: null,
+  rootMargin: "10% 0px",
+  threshold: [0],
+};
+
+// The callback receives a list of IntersectionObserverEntry objects and the observer
+const targets = (entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target);
+    }
   });
+};
+
+// Create the intersection observer by calling its constructor.
+// trigger this callback 'targets' when scrolled and passed by fadeOption.
+const fadeObserver = new IntersectionObserver(targets, fadeOption);
+
+// call fadeObserver for each fadeTarget
+fadeTargets.forEach((target) => {
+  fadeObserver.observe(target);
 });
